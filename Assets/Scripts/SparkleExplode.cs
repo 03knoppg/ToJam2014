f2ded22e41f5058caaf46d20a97a78af46278719
Graham <graham.knopp@gmail.com>
2014-04-25 23:59:10 -0400

    Explosions!!

---
+++ Assets/Scripts/SparkleExplode.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SparkleExplode : MonoBehaviour {
+
+	ParticleEmitter emitter;
+
+	//seconds
+	public float duration;
+	float t;
+
+	// Use this for initialization
+	void Start () {
+
+		emitter = gameObject.GetComponent<ParticleEmitter>();
+
+		t = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t = Mathf.Clamp01(t + Time.deltaTime / duration);
+		
+		emitter.minEmission = Mathf.Lerp(20, 100, t);
+		emitter.maxEmission = Mathf.Lerp(20, 100, t);
+
+		//time to die
+		if(t >= 0.3f)
+		{
+			emitter.emit = false;
+			gameObject.transform.parent.renderer.enabled = false;
+		}
+		if(t == 1)
+		{
+			Destroy(gameObject.transform.parent.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/TestButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButtons : MonoBehaviour {
+
+	public GameObject exploder;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+		if(GUI.Button(new Rect(500, 5, 50,50), "Explode")){
+			GameObject go = (GameObject) GameObject.Instantiate(exploder);
+			go.transform.parent = GameObject.Find("Cube").transform;
+			go.transform.localPosition = Vector3.zero;
+		}
+	}
+}
