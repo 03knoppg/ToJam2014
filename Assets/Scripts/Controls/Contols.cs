542a1489a73ceb0d153d173cd7774324d4ab5808
Meeka <diana.compagnone@gmail.com>
2014-04-26 19:08:36 -0400

    Diana's changes

--- Assets/Scripts/Controls/Contols.cs
+++ Assets/Scripts/Controls/Contols.cs
@@ -17,49 +17,54 @@ public class Contols : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		updateSpeed ();
+		updateRotation ();
+	}
+
+	void updateSpeed(){
+
 		float x = 0;
 		float y = 0;
 		float z = 0;
-
+		
 		//slows down if no key pressed
 		velocity = velocity*0.9f;
 		if (velocity.sqrMagnitude < new Vector3(0.01f, 0.01f, 0.01f).sqrMagnitude)
 			velocity = new Vector3 (0, 0, 0);
-
-
+		
 		//handle button presses
 		if (Input.GetKey ("w")) {
 			print("w pressed");
 			y = speed;
-			updateSpeed( x, y, z );
+			velocity = new Vector3( x, y, z );
 		} 
-
+		
 		if (Input.GetKey ("s")) {
 			print ("s pressed");
 			y = -speed;
-			updateSpeed (x, y, z);
+			velocity = new Vector3( x, y, z );
 		}
-
+		
 		if (Input.GetKey ("a")) {
 			print ("a pressed");
-			x = speed;
-			updateSpeed( x, y, z );
+			x = -speed;
+			velocity = new Vector3( x, y, z );
 		}
-
+		
 		if (Input.GetKey ("d")) {
 			print("d pressed");
-			x = -speed;
-			updateSpeed( x, y, z );
+			x = speed;
+			velocity = new Vector3( x, y, z );
 		}
-
+		
 		//update object speed
 		print (transform.position);
 		transform.position = transform.position + velocity;
 	}
 
-	void updateSpeed(float x, float y, float z){
+	void updateRotation(){
 
-		velocity = new Vector3( x, y, z );
+
 
 	}
 }
